78c872434e9552d896d361a80e783aef61723a33
lucaruggeri <lucaruggeri@hotmail.com>
2016-10-31 00:17:07 +0100

    First commit
    First commit

---
+++ PicturesFromMars/PicturesFromMars/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace PicturesFromMars
+{
+    [Activity(Label = "PicturesFromMars", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            Spinner spinner = FindViewById<Spinner>(Resource.Id.spinner);
+            spinner.ItemSelected += new System.EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+            var adapter = ArrayAdapter.CreateFromResource(this, Resource.Array.cameras_array, Android.Resource.Layout.SimpleSpinnerItem);
+            adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            spinner.Adapter = adapter;
+        }
+
+        private void spinner_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            Spinner spinner = (Spinner)sender;
+            string toast = string.Format("The camera is {0}", spinner.GetItemAtPosition(e.Position));
+            Toast.MakeText (this, toast, ToastLength.Long).Show();
+        }
+    }
+}
+
